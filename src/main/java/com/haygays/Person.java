9ca68bc3427daf5a7013bded7e4b6e4e16d3b6ee
qaayy444 <qaayy444@gmail.com>
2021-11-07 18:24:58 +0800

    java,kotlin data type

--- src/main/java/com/haygays/Person.java
+++ src/main/java/com/haygays/Person.java
@@ -32,6 +32,7 @@ public class Person {
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(nums[i])) {
                 return new int[]{map.get(nums[i]), i};
+
             }
             System.out.println(target - nums[i]);
             map.put(target - nums[i], i);
--- src/main/kotlin/com/haygays/hello.kt
+++ src/main/kotlin/com/haygays/hello.kt
@@ -6,6 +6,9 @@ fun main(array: Array<String>) {
     //kotlin建構class時不用加new
     val p=People()
     p.hello()
+    //val:不可變;var:可變
+    //若之後才要賦予變數值，必須先給定該變數的資料型態才行，不然kotlin無法分辨(如下的標示)
+    val age :Int=18
 
 }
 class People
